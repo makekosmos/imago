278df4f70cf377749e3d819305928871d899602d
ksanrse <ksanrse@gmail.com>
2026-09-11 19:27:00 +0300

    few things

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,3 +1,9 @@
-export default function Button() {
+interface ButtonProps extends React.ComponentProps<"button">, VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
 
+export default function Button({ className, ...props }) {
+  const Comp = asChild ? Slot.root : "button";
+
+  return <Comp {...props} />;
 }
